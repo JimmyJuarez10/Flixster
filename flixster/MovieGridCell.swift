50b0400b23abd7ddcef74b775328095c4958d99e
Jimmy Juarez <jimjuareza@gmail.com>
2021-04-13 00:24:24 -0700

    Flix pt2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jimmy on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
